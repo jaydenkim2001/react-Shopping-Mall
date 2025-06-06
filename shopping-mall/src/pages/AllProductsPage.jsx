ab84270379147ec28f9836e5f2f64669e28e459f
jaydenkim2001 <92327600+jaydenkim2001@users.noreply.github.com>
2025-06-06 11:24:32 +0000

    6 June 2025: Created a json server to store the products data

--- shopping-mall/src/pages/AllProductsPage.jsx
+++ shopping-mall/src/pages/AllProductsPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const AllProductsPage = () => {
+  const getProducts = async () => {
+    let url = `/api/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  }
+  useEffect(()=>{
+    getProducts();
+  }, [])
   return (
-    <div>AllProductsPage</div>
+    <div></div>
   )
 }
 
