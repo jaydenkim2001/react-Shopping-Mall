1aef95207fbf27e7306cea93594c45e218daffee
jaydenkim2001 <92327600+jaydenkim2001@users.noreply.github.com>
2025-06-07 07:34:28 +0000

    7 June 2025

---
+++ shopping-mall/src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://image.hm.com/assets/hm/ab/b9/abb979d2bab2ac4aaace6bb64b6aa780536a4c05.jpg?imwidth=1536' style={{ width: '250px', height: '350px' }}/>
+        <div>Conscious Choice</div>
+        <div>Product Name</div>
+        <div>Price</div>
+        <div>New or Old Product</div>
+    </div>
+  )
+}
+
+export default ProductCard
+
--- shopping-mall/src/pages/AllProductsPage.jsx
+++ shopping-mall/src/pages/AllProductsPage.jsx
@@ -1,17 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard';
 
 const AllProductsPage = () => {
+  const [productList, setProductList] = useState([]);
+
   const getProducts = async () => {
     let url = `/api/products`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    //console.log(data);
+    setProductList(data);
   }
   useEffect(()=>{
     getProducts();
   }, [])
   return (
-    <div></div>
+    <div>
+      <ProductCard/>
+    </div>
   )
 }
 
